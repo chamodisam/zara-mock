77fc0495de1b1fb3fb37b6339b925ee1b7d59f60
chamodisam <chamodi@codify.ai>
2025-05-01 23:48:31 +0530

    Clean up hardcoded cart init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import { useEffect, useCallback, useContext } from 'react';
-import axios from 'axios';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Help from './pages/Help';
@@ -10,20 +8,7 @@ import Cart from './pages/Cart';
 import Login from './auth/Login';
 import Signup from './auth/Signup';
 
-import CartContext from './contexts/cart';
-
 function App() {
-  const { setCartItems, setTotalQuantity } = useContext(CartContext);
-
-  const fetchCartItems = useCallback(async () => {
-    const response = await axios.get('http://localhost:3001/carts/20');
-    setCartItems(response.data.items);
-    setTotalQuantity(response.data.total_quantity);
-  }, []);
-
-  useEffect(() => {
-    fetchCartItems();
-  }, [fetchCartItems]);
 
   return (
     <Router>
